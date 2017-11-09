4188dc428702ad1f725e12b296ab17963093b8b7
alucas- <lucas.abel@yandex.com>
2017-11-09 14:12:57 +0100

    cmake integration

--- libft/test_strsplit.c
+++ libft/test_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 15:37:09 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/08 16:05:49 by alucas-          ###   ########.fr       */
+/*   Updated: 2017/11/09 14:09:40 by alucas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,15 @@ t_i32	main(void)
 		{ "*hello**fellow", '*', {"hello", "fellow", NULL} },
 		{ "coucou*toi**", '*', {"coucou", "toi", NULL} },
 		{ "coucou*tu**veux", '*', {"coucou", "tu", "veux", NULL} },
+		{ "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim"
+          " sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, variu"
+          "s a, semper congue, euismod non, mi.", '*', { NULL } },
+		{ "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim"
+          " sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, variu"
+          "s a, semper congue, euismod non, mi.", 'z', { NULL } },
 	};
 
-	for (int i = 0; i < 3; ++i)
+	for (int i = 0; i < 4; ++i)
 	{
 		t_car **ss = ft_strsplit(tests->a, tests->s);
 		t_car **sst = tests->b;

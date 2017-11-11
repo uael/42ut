8617794a82c666c7e29248181da851433667ab0f
alucas- <lucas.abel@yandex.com>
2017-11-11 12:21:06 +0100

    fixed itoa base

--- libft/test_itoa_base.c
+++ libft/test_itoa_base.c
@@ -6,23 +6,18 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 15:37:09 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/09 14:09:40 by alucas-          ###   ########.fr       */
+/*   Updated: 2017/11/09 08:05:27 by alucas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <assert.h>
-#include <fcntl.h>
 #include <libft.h>
 
 t_i32	main(void)
 {
-	t_ifs	ifs;
-	t_car	*buf;
-
-	ifs.fd = open("CMakeLists.txt", O_RDONLY);
-	ifs.buf = NULL;
-	while (ft_ifsgetln(&ifs, &buf))
-		ft_putendl(buf);
-	close(ifs.fd);
+	assert(ft_strequ(ft_itoa_base(10, 2), "1010"));
+	assert(ft_strequ(ft_itoa_base(42, 2), "101010"));
+	assert(ft_strequ(ft_itoa_base(564, 2), "1000110100"));
+	assert(ft_strequ(ft_itoa_base(515616516, 30), "l6gr76"));
 	return (EXIT_SUCCESS);
 }

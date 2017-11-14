35db90a06bf4be86f8e4a8c0df5aa16f2ec57f79
alucas- <lucas.abel@yandex.com>
2017-11-14 18:18:21 +0100

    eog => end of GNL yay fillit: fuck it

--- gnl/test_gnl1.c
+++ gnl/test_gnl1.c
@@ -6,19 +6,21 @@
 /*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 15:37:09 by alucas-           #+#    #+#             */
-/*   Updated: 2017/11/09 16:58:20 by alucas-          ###   ########.fr       */
+/*   Updated: 2017/11/14 17:04:46 by alucas-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <assert.h>
 #include <libft.h>
 #include <get_next_line.h>
+#include <fcntl.h>
 
-t_i32	main(void)
+t_i32	main(int ac, char **av)
 {
-	char *bla;
+	char	*bla;
+	int		fd = open(av[1], O_RDONLY);
 
-	while (get_next_line(0, &bla))
+	while (get_next_line(fd, &bla))
 		ft_putendl(bla), free(bla);
 	return (EXIT_SUCCESS);
 }

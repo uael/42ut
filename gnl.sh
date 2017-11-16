c07d698a5eff66f0d86c81367b742da5aac32f6a
alucas- <alucas-@student.42.fr>
2017-11-16 14:02:00 +0100

    gnl tests

---
+++ gnl.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+source 'lib.sh'
+
+PROJECT_PATH=$1
+
+function donorm {
+  local pids=""
+  norm_file ${PROJECT_PATH}/get_next_line.h &
+  pids="$pids $!"
+  norm_file ${PROJECT_PATH}/get_next_line.c &
+  pids="$pids $!"
+  wait ${pids}
+  out=$(cat ${NORM_OUT})
+  if [[ ! -z ${out} ]]; then
+    echo -e "\n$out"
+    exit 1
+  fi
+  exit 0
+}
+
+function dotest {
+  local test=$1
+  clang -Wall -Wextra -Werror -I ${PROJECT_PATH}/libft/includes -oout/gnl/$(basename "${test%.*}").o -c ${test}
+  clang -oout/gnl/$(basename "${test%.*}") out/gnl/$(basename "${test%.*}").o out/gnl/get_next_line.o -I ${PROJECT_PATH}/libft/includes -L ${PROJECT_PATH}/libft/ -lft
+  ./out/gnl/$(basename "${test%.*}")
+}
+
+mkdir -p out/gnl
+job "Make" "make libft all" "make -C${PROJECT_PATH}/libft all"
+job "Make" "make libft clean" "make -C${PROJECT_PATH}/libft clean"
+job "Make" "compile" "clang -Wall -Wextra -Werror -I ${PROJECT_PATH}/libft/includes -o out/gnl/get_next_line.o -c ${PROJECT_PATH}/get_next_line.c"
+for test in ./gnl/test_*.c; do
+  job "Test" "$(basename "${test%.*}")" "dotest ${test}"
+done
+job "Norm" "all" "donorm"
+
+rm ${OUT}
+rm ${NORM_OUT}
--- gnl/test_gnl1.c
+++ gnl/test_gnl1.c
@@ -15,13 +15,44 @@
 #include "src/get_next_line.h"
 #include <fcntl.h>
 
+static char *foo[] = {
+	"This is free and unencumbered software released into the public domain.",
+	"",
+	"Anyone is free to copy, modify, publish, use, compile, sell, or",
+	"distribute this software, either in source code form or as a compiled",
+	"binary, for any purpose, commercial or non-commercial, and by any",
+	"means.",
+	"",
+	"In jurisdictions that recognize copyright laws, the author or authors",
+	"of this software dedicate any and all copyright interest in the",
+	"software to the public domain. We make this dedication for the benefit",
+	"of the public at large and to the detriment of our heirs and",
+	"successors. We intend this dedication to be an overt act of",
+	"relinquishment in perpetuity of all present and future rights to this",
+	"software under copyright law.",
+	"",
+	"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,",
+	"EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF",
+	"MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.",
+	"IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR",
+	"OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,",
+	"ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR",
+	"OTHER DEALINGS IN THE SOFTWARE.",
+	"",
+	"For more information, please refer to <http://unlicense.org>",
+	NULL
+};
+
 int	main(int ac, char **av)
 {
 	char	*bla;
-	int		fd = open(av[1], O_RDONLY);
+	char 	**bar;
+	int		fd = open("LICENSE", O_RDONLY);
 
 	(void)ac;
+	(void)av;
+	bar = foo;
 	while (get_next_line(fd, &bla) > 0)
-		ft_putendl(bla), free(bla);
+		assert(STREQ(*bar, bla)), ++bar, free(bla);
 	return (EXIT_SUCCESS);
 }
